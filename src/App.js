87d858a017f7544e496671c667daf530c56bcc43
Manuel Henriquez <manuelhm1993@gmail.com>
2023-08-23 19:40:53 -0400

    Personalizar el componente principal src/App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+// Componente react regla PascalCase
 function App() {
+  // La función de componente siempre debe retornar un contenido para renderizarlo
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // Todo el contenido a renderizar debe ir dentro de un contenedor padre div, React.Fragment o la expresión <></>
+    <div>
+      {/* Contenido a renderizar */}
+      <h1>Hola mundo React</h1>
     </div>
   );
 }
